a07141634cd182bc38a52491061b29e17d8bbd5b
REZA DYSTA SATRIA <87264688+dystaSatria@users.noreply.github.com>
2023-07-11 21:09:57 +0300

    Update brainfuckTranslator.py

--- brainfuckTranslator.py
+++ brainfuckTranslator.py
@@ -1,4 +1,4 @@
-def translate_to_brainfuck(input_text):
+def translateBrainfuck(input_text):
     brainfuck_code = ""
     for char in input_text:
         ascii_value = ord(char)
@@ -7,12 +7,12 @@ def translate_to_brainfuck(input_text):
     return brainfuck_code
 
 while True:
-    user_input = input("Input your text for translates to Brainfuck Language (Input 'Exit' for end the program): ")
+    userInput = input("Input your text for translates to Brainfuck Language (Input 'Exit' for end the program): ")
 
-    if user_input.lower() == "exit":
+    if userInput.lower() == "exit":
         break
 
-    brainfuck_program = translate_to_brainfuck(user_input)
+    brainfuck_program = translate_to_brainfuck(userInput)
 
     print("Result as Brainf#ck Languange :")
     print(brainfuck_program)
